d230f6ad0fb1dc7e9bc446925d8f12c871790840
Julian Cataño <julianestivenposso@gmail.com>
2025-07-24 13:38:16 -0500

    identificador de tipo de rol

--- src/layouts/SidebarLayouts.jsx
+++ src/layouts/SidebarLayouts.jsx
@@ -1,9 +1,8 @@
 import { Cookie, User } from "lucide-react";
 import LogoutButton from "../components/LogoutButton";
 import { useState } from "react";
-import MenuUser from "../components/MenuUser"
+import MenuUser from "../components/MenuUser";
 import Cookies from "js-cookie";
-
 
 export default function Layout({ children }) {
   const [showMenu, setShowMenu] = useState(false);
@@ -16,9 +15,22 @@ export default function Layout({ children }) {
           <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center text-emerald-600 font-bold text-lg mr-4">
             ULA
           </div>
+          <p
+            className={`inline-block px-3 py-1 text-xs font-medium rounded-full border ${
+              Cookies.get("userRol") === "administrador"
+                ? "bg-green-50 text-green-700 border-green-300"
+                : Cookies.get("userRol") === "usuario"
+                ? "bg-blue-50 text-blue-700 border-blue-300"
+                : "bg-gray-100 text-gray-600 border-gray-300"
+            }`}
+          >
+            {Cookies.get("userRol")}
+          </p>
         </div>
         <div className="flex items-center gap-4">
-          <span className="text-sm text-gray-500">{Cookies.get("userEmail")}</span>
+          <span className="text-sm text-gray-500">
+            {Cookies.get("userEmail")}
+          </span>
           <div className="relative inline-block text-left">
             <div
               className="w-8 h-8 bg-green-600 rounded-full flex items-center justify-center cursor-pointer"
@@ -37,10 +49,16 @@ export default function Layout({ children }) {
         {/* Sidebar redondeada a la izquierda */}
         <aside className="w-56 bg-green-600 text-white p-4 rounded-l-xl">
           <nav className="space-y-2">
-            <a href="/ListFiles" className="block hover:bg-green-500 px-4 py-3 rounded">
+            <a
+              href="/ListFiles"
+              className="block hover:bg-green-500 px-4 py-3 rounded"
+            >
               Gestión de archivos
             </a>
-            <a href="/MineFiles" className="block hover:bg-green-500 px-4 py-3 rounded">
+            <a
+              href="/MineFiles"
+              className="block hover:bg-green-500 px-4 py-3 rounded"
+            >
               Historial de Archivos
             </a>
             <a href="/" className="block hover:bg-green-500 px-4 py-3 rounded">
